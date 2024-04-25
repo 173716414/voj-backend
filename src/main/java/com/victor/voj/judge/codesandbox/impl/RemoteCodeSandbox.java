36bc66d21f62badfb99b9d1b8411d43f7e894f3e
Victor <47047497+173716414@users.noreply.github.com>
2024-04-25 16:53:56 +0800

    示例代码沙箱、远程代码沙箱、第三方代码沙箱 测试示例代码沙箱

---
+++ src/main/java/com/victor/voj/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -0,0 +1,16 @@
+package com.victor.voj.judge.codesandbox.impl;
+
+import com.victor.voj.judge.codesandbox.CodeSandbox;
+import com.victor.voj.judge.codesandbox.model.ExecuteCodeRequest;
+import com.victor.voj.judge.codesandbox.model.ExecuteCodeResponse;
+
+/**
+ * 远程代码沙箱（实际调用接口的沙箱）
+ */
+public class RemoteCodeSandbox implements CodeSandbox {
+    @Override
+    public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
+        System.out.println("远程代码沙箱");
+        return null;
+    }
+}
