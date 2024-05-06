f385409950d902e702c503b6fc462a98a59286c6
Victor <47047497+173716414@users.noreply.github.com>
2024-05-06 16:44:44 +0800

    代码服务接口和实现

---
+++ src/main/java/com/victor/voj/judge/JudgeService.java
@@ -0,0 +1,16 @@
+package com.victor.voj.judge;
+
+import com.victor.voj.model.entity.QuestionSubmit;
+
+/**
+ * 判题服务
+ */
+public interface JudgeService {
+
+    /**
+     * 判题
+     * @param questionSubmitId
+     * @return
+     */
+    QuestionSubmit doJudge(long questionSubmitId);
+}
