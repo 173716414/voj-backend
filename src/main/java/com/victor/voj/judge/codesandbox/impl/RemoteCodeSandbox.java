74e8524901e3fddfbde82c2afbeea4a919bcac75
Victor <47047497+173716414@users.noreply.github.com>
2024-05-13 15:39:09 +0800

    远程代码沙箱

--- src/main/java/com/victor/voj/judge/codesandbox/impl/RemoteCodeSandbox.java
+++ src/main/java/com/victor/voj/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -1,8 +1,13 @@
 package com.victor.voj.judge.codesandbox.impl;
 
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.victor.voj.common.ErrorCode;
+import com.victor.voj.exception.BusinessException;
 import com.victor.voj.judge.codesandbox.CodeSandbox;
 import com.victor.voj.judge.codesandbox.model.ExecuteCodeRequest;
 import com.victor.voj.judge.codesandbox.model.ExecuteCodeResponse;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 远程代码沙箱（实际调用接口的沙箱）
@@ -11,6 +16,15 @@ public class RemoteCodeSandbox implements CodeSandbox {
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         System.out.println("远程代码沙箱");
-        return null;
+        String url = "http://localhost:8090/executeCode";
+        String json = JSONUtil.toJsonStr(executeCodeRequest);
+        String responseStr = HttpUtil.createPost(url)
+                .body(json)
+                .execute()
+                .body();
+        if (StringUtils.isBlank(responseStr)) {
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR, "executeCode remoteSandbox error, message = " + responseStr);
+        }
+        return JSONUtil.toBean(responseStr, ExecuteCodeResponse.class);
     }
 }
